bfa7bd3f7c103c64ad7ad43549c4d87d395fed9e
Andrei Katin <andrei@yourdomain>
2026-01-10 02:30:36 +0300

    Update base URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.NOWAR_MAPS_KEY = 'AIzaSyD_4QZHoESCH_RR_qtCurnBBiKoNuMOMQ8';
 window.NOWAR_TRANSLATE_KEY = 'AIzaSyC5m1mWqMprbt4_PZQ8CVDsGNVyohWbgNo';
 window.NOWAR_TRANSLATE_ENDPOINT = 'https://translation.googleapis.com/language/translate/v2';
-window.NOWAR_BASE_URL = '.';
+window.NOWAR_BASE_URL = 'in.drive.ru';
